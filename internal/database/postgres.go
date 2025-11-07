2c9118bf0115b8f195e9b7da8a88d25f6e614688
phamhoa2416 <phamviethoa2416@gmail.com>
2025-11-07 20:14:25 +0700

    fix: migrate from database sql to gorm

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -1,30 +1,41 @@
 package database
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"logistics-quality-monitor/internal/config"
 	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type Database struct {
-	DB *sql.DB
+	DB *gorm.DB
 }
 
 func NewDatabase(cfg *config.Config) (*Database, error) {
-	db, err := sql.Open("postgres", cfg.Database.DSN())
+	dsn := cfg.Database.DSN()
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info), // In log SQL ra terminal
+	})
 	if err != nil {
 		return nil, fmt.Errorf("error opening database: %w", err)
 	}
 
-	db.SetMaxOpenConns(25)
-	db.SetMaxIdleConns(5)
-	db.SetConnMaxLifetime(5 * time.Minute)
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("error getting sql.DB: %w", err)
+	}
 
-	if err := db.Ping(); err != nil {
+	sqlDB.SetMaxOpenConns(25)
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+
+	if err := sqlDB.Ping(); err != nil {
 		return nil, fmt.Errorf("error connecting to database: %w", err)
 	}
 
@@ -34,9 +45,17 @@ func NewDatabase(cfg *config.Config) (*Database, error) {
 }
 
 func (d *Database) Close() error {
-	return d.DB.Close()
+	sqlDB, err := d.DB.DB()
+	if err != nil {
+		return err
+	}
+	return sqlDB.Close()
 }
 
 func (d *Database) Health() error {
-	return d.DB.Ping()
+	sqlDB, err := d.DB.DB()
+	if err != nil {
+		return err
+	}
+	return sqlDB.Ping()
 }
