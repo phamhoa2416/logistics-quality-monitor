665437778023fc28da5e4b44c1a81df32661f05c
phamhoa2416 <phamviethoa2416@gmail.com>
2025-11-08 22:26:27 +0700

    feat: add indexes for refresh_token table

--- migrations/users/04_add_indexes.up.sql
+++ migrations/users/04_add_indexes.up.sql
@@ -11,5 +11,11 @@ CREATE INDEX IF NOT EXISTS idx_password_reset_tokens_expires_at ON password_rese
 CREATE INDEX IF NOT EXISTS idx_password_reset_tokens_used ON password_reset_tokens (used);
 CREATE INDEX IF NOT EXISTS idx_password_reset_tokens_user_id_used ON password_reset_tokens (user_id, used) WHERE used = false;
 
+CREATE INDEX IF NOT EXISTS idx_refresh_tokens_user_id ON refresh_tokens (user_id);
+CREATE INDEX IF NOT EXISTS idx_refresh_tokens_token ON refresh_tokens (token);
+CREATE INDEX IF NOT EXISTS idx_refresh_tokens_expires_at ON refresh_tokens (expires_at);
+CREATE INDEX IF NOT EXISTS idx_refresh_tokens_revoked ON refresh_tokens (revoked);
+CREATE INDEX IF NOT EXISTS idx_refresh_tokens_user_revoked ON refresh_tokens (user_id, revoked) WHERE revoked = false;
+
 COMMENT ON INDEX idx_users_email IS 'Index to optimize lookups by email in users table.';
 COMMENT ON INDEX idx_password_reset_tokens_token IS 'Index to optimize lookups by token in password_reset_tokens table.';
