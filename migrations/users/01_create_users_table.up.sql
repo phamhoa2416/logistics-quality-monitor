03d840561097b2cd5b73b4d11956606370015849
phamhoa2416 <phamviethoa2416@gmail.com>
2025-11-08 22:26:56 +0700

    chore: format the SQL code

--- migrations/users/01_create_users_table.up.sql
+++ migrations/users/01_create_users_table.up.sql
@@ -3,26 +3,28 @@ CREATE TYPE user_role AS ENUM (
     'provider',
     'shipper',
     'admin'
-);
+    );
 
-CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    username VARCHAR(100) UNIQUE NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password_hashed VARCHAR(255) NOT NULL,
-    full_name VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(20) UNIQUE,
-    role user_role NOT NULL DEFAULT 'customer',
+CREATE TABLE users
+(
+    id              UUID PRIMARY KEY                  DEFAULT gen_random_uuid(),
+    username        VARCHAR(100) UNIQUE      NOT NULL,
+    email           VARCHAR(255) UNIQUE      NOT NULL,
+    password_hashed VARCHAR(255)             NOT NULL,
+    full_name       VARCHAR(255)             NOT NULL,
+    phone_number    VARCHAR(20) UNIQUE,
+    role            user_role                NOT NULL DEFAULT 'customer',
 
-    address TEXT,
+    address         TEXT,
 
-    is_active BOOLEAN NOT NULL DEFAULT TRUE,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+    is_active       BOOLEAN                  NOT NULL DEFAULT TRUE,
+    created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS
+$$
 BEGIN
     NEW.updated_at = NOW();
     RETURN NEW;
@@ -30,8 +32,9 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER update_users_updated_at
-BEFORE UPDATE ON users
-FOR EACH ROW
+    BEFORE UPDATE
+    ON users
+    FOR EACH ROW
 EXECUTE PROCEDURE update_updated_at_column();
 
 COMMENT ON TABLE users IS 'Table storing user information with 4 roles: customer, provider, shipper, admin.';
